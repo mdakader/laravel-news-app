3990072ccf5754e5950c1470afb144aa827aa8d1
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-05 08:27:07 +0600

    Admin Setup - Adding Multi Auth System with Breeze (part-1 + 2)
    Admin Setup - Adding Multi Auth System with Breeze (part-1 + 2)
    
    - set middleware and check admin guard

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminAuthenticationController;
 use App\Http\Controllers\Admin\DashboardController;
 
-//Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['admin']], function(){
-//    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-//});
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
+    Route::get('login', [AdminAuthenticationController::class, 'login'])->name('login');
+});
 
 
-//Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function(){
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
