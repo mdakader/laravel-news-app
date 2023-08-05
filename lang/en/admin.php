fdd06c7a11a707140c3f7a658fb92863fa6880ea
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-05 10:59:12 +0600

    Admin Setup- Adding localization syntax in all static text
    Admin Setup- Adding localization syntax in all static text
    
    - Cli - php artisan lang:publish
    
    - Example - {{ __('admin.Login') }}

---
+++ lang/en/admin.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Login' => 'Login',
+    'Forgot Password' => 'Forgot Password',
+    'Forgot your password? No problem. We got you.' => 'Forgot your password? No problem. We got you.',
+    'Email' => 'Email',
+    'Please fill in your email' => 'Please fill in your email',
+    'Send Link' => 'Send Link',
+    'Copyright' => 'Copyright',
+    'WebSolutionUs 2023' => 'WebSolutionUs 2023',
+    'Password' => 'Password',
+    'Forgot Password?' => 'Forgot Password?',
+    'please fill in your password' => 'please fill in your password',
+    'Remember Me' => 'Remember Me',
+    'Reset Password' => 'Reset Password',
+    'Please fill in your password' => 'Please fill in your password',
+    'Confirmation Password' => 'Confirmation Password',
+    'Please fill in your confirmation password' => 'Please fill in your confirmation password',
+];
