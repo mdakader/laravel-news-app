bbc9de4f709dce1c779305b9e4f18e18d809f7df
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-05 07:44:44 +0600

    Laravel News App

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,58 @@
+<div class="navbar-bg"></div>
+<!-- Navbar Start -->
+@include('admin.layouts.navbar')
+<!-- Navbar End -->
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="dropdown active">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                    <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Starter</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+                </ul>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Stisla</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Components</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="components-article.html">Article</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
+                </ul>
+            </li>
+
+        </ul>
+
+        <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                <i class="fas fa-rocket"></i> Documentation
+            </a>
+        </div>
+    </aside>
+</div>
---
+++ routes/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+
+//Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['admin']], function(){
+//    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+//});
+
+
+//Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
