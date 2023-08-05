fcc3266a2c857317d5c95e52daeee40bb03f3c5f
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-05 16:55:50 +0600

    Multi Language- Handling Create Form Data (Part - 1)
    Multi Language- Handling Create Form Data (Part - 1)

--- resources/views/admin/language/create.blade.php
+++ resources/views/admin/language/create.blade.php
@@ -12,24 +12,34 @@
 
             </div>
             <div class="card-body">
-                <form action="" method="POST">
+                <form action="{{ route('admin.language.store') }}" method="POST">
                     @csrf
                     <div class="form-group">
                         <label for="">{{ __('Language') }}</label>
                         <select name="lang" id="language-select" class="form-control select2">
                             <option value="">--{{ __('Select') }}--</option>
-                                <option value=""></option>
+                            @foreach (config('language') as $key => $lang)
+                                <option value="{{ $key }}">{{ $lang['name'] }}</option>
+                            @endforeach
                         </select>
+                        @error('lang')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
 
                     </div>
                     <div class="form-group">
                         <label for="">{{ __('Name') }}</label>
                         <input readonly name="name" type="text" class="form-control" id="name">
-
+                        @error('name')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label for="">{{ __('Slug') }}</label>
                         <input readonly name="slug" type="text" class="form-control" id="slug">
+                        @error('slug')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label for="">{{ __('Is it default?') }} </label>
@@ -37,6 +47,9 @@
                             <option value="0">{{ __('No') }}</option>
                             <option value="1">{{ __('Yes') }}</option>
                         </select>
+                        @error('default')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label for="">{{ __('Status') }}</label>
@@ -44,6 +57,9 @@
                             <option value="1">{{ __('Active') }}</option>
                             <option value="0">{{ __('Inactive') }}</option>
                         </select>
+                        @error('status')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">{{ __('Create') }}</button>
                 </form>
@@ -51,3 +67,17 @@
         </div>
     </section>
 @endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('#language-select').on('change', function() {
+                let value = $(this).val();
+                let name = $(this).children(':selected').text();
+                $('#slug').val(value);
+                $('#name').val(name);
+
+            })
+        })
+    </script>
+@endpush
