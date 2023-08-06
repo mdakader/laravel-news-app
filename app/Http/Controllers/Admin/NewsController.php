4d1cfeb9ad455238f2bbed9360b93707b5d29860
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-06 09:13:25 +0600

    News Post - Working With Delete Feature
     News Post - Working With Delete Feature

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -174,7 +174,12 @@ class NewsController extends Controller
      */
     public function destroy(string $id)
     {
+        $news = News::findOrFail($id);
+        $this->deleteFile($news->image);
+        $news->tags()->delete();
+        $news->delete();
 
+        return response(['status' => 'success', 'message' => __('Deleted Successfully!')]);
     }
 
 }
