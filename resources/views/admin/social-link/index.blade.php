0ad02d676f3be7eb0797087375f5ef4eb856c647
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-08 19:45:50 +0600

    Social Link - Showing Data at Index Page
    Social Link - Showing Data at Index Page

--- resources/views/admin/social-link/index.blade.php
+++ resources/views/admin/social-link/index.blade.php
@@ -3,15 +3,15 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>{{ __('Social Links') }}</h1>
+            <h1>{{ __('admin.Social Links') }}</h1>
         </div>
 
         <div class="card card-primary">
             <div class="card-header">
-                <h4>{{ __('All social links') }}</h4>
+                <h4>{{ __('admin.All link') }}</h4>
                 <div class="card-header-action">
                     <a href="{{ route('admin.social-link.create') }}" class="btn btn-primary">
-                        <i class="fas fa-plus"></i> {{ __('Create new') }}
+                        <i class="fas fa-plus"></i> {{ __('admin.Create new') }}
                     </a>
                 </div>
             </div>
@@ -24,25 +24,41 @@
                             <th class="text-center">
                                 #
                             </th>
-                            <th>{{  __('Email') }}</th>
+                            <th>{{ __('admin.Icon') }}</th>
 
-                            <th>{{  __('Action') }}</th>
+                            <th>{{ __('admin.Url') }}</th>
+                            <th>{{ __('admin.Status') }}</th>
+
+                            <th>{{ __('admin.Action') }}</th>
+
                         </tr>
                         </thead>
                         <tbody>
-{{--                        @foreach ($subs as $sub)--}}
-{{--                            <tr>--}}
-{{--                                <td>{{ $sub->id }}</td>--}}
-{{--                                <td>{{ $sub->email }}</td>--}}
 
+                        @foreach ($socialLinks as $link)
+                            <tr>
+                                <td>{{ ++$loop->index }}</td>
+                                <td><i style="font-size:30px" class="{{ $link->icon }}"></i></td>
+                                <td>{{ $link->url }}</td>
+                                <td>
+                                    @if($link->status === 1)
+                                        <span class="badge badge-success">{{ __('admin.Yes') }}</span>
+                                    @else
+                                        <span class="badge badge-danger">{{ __('admin.No') }}</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="{{ route('admin.social-link.edit', $link->id) }}"
+                                       class="btn btn-primary"><i class="fas fa-edit"></i></a>
+                                    <a href="{{ route('admin.social-link.destroy', $link->id) }}"
+                                       class="btn btn-danger delete-item"><i
+                                            class="fas fa-trash-alt"></i></a>
 
-{{--                                <td>--}}
-{{--                                    <a href="{{ route('admin.subscribers.destroy', $sub->id) }}"--}}
-{{--                                       class="btn btn-danger delete-item"><i class="fas fa-trash-alt"></i></a>--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
+                                </td>
 
+                            </tr>
+
+                        @endforeach
 
                         </tbody>
                     </table>
