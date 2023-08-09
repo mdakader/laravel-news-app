b9ddba4ebcf4bfa737505b44985a03be15dfdb1f
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-09 17:32:41 +0600

    Role Management - Installing Laravel Spatie Permission Package
    Role Management - Installing Laravel Spatie Permission Package

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
