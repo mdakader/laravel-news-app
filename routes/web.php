90437fad4955df4633edc691f77f88d35a7a893d
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-09 12:31:54 +0600

    Contact Page - Working with Contact Form
    Contact Page - Working with Contact Form

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,5 @@ Route::get('about', [HomeController::class, 'about'])->name('about');
 /** Contact Page Route */
 Route::get('contact', [HomeController::class, 'contact'])->name('contact');
 
+/** Contact Page Route */
+Route::post('contact', [HomeController::class, 'handleContactFrom'])->name('contact.submit');
