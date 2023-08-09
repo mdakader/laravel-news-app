bf756b094135cf8c11a8ea4c0ba7ff21d11e7014
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-10 02:15:16 +0600

    News Section - Show News Depending on Admin id and permissions
    News Section - Show News Depending on Admin id and permissions

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -3,15 +3,15 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>{{ __('News') }}</h1>
+            <h1>{{__('News') }}</h1>
         </div>
 
         <div class="card card-primary">
             <div class="card-header">
-                <h4>{{ __('All News') }}</h4>
+                <h4>{{__('All News') }}</h4>
                 <div class="card-header-action">
                     <a href="{{ route('admin.news.create') }}" class="btn btn-primary">
-                        <i class="fas fa-plus"></i> {{ __('Create new') }}
+                        <i class="fas fa-plus"></i> {{__('Create new') }}
                     </a>
                 </div>
             </div>
@@ -20,8 +20,7 @@
                 <ul class="nav nav-tabs" id="myTab2" role="tablist">
                     @foreach ($languages as $language)
                         <li class="nav-item">
-                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2"
-                               data-toggle="tab"
+                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2" data-toggle="tab"
                                href="#home-{{ $language->lang }}" role="tab" aria-controls="home"
                                aria-selected="true">{{ $language->name }}</a>
                         </li>
@@ -31,12 +30,20 @@
                 <div class="tab-content tab-bordered" id="myTab3Content">
                     @foreach ($languages as $language)
                         @php
-
-                            $news = \App\Models\News::with('category')
-                            ->where('language', $language->lang)
-                            ->orderBy('id', 'DESC')
-                            ->get();
-
+                            if(canAccess(['news all-access'])){
+                                $news = \App\Models\News::with('category')
+                                ->where('language', $language->lang)
+                                ->where('is_approved', 1)
+                                ->orderBy('id', 'DESC')
+                                ->get();
+                            }else {
+                                $news = \App\Models\News::with('category')
+                                ->where('language', $language->lang)
+                                ->where('is_approved', 1)
+                                ->where('auther_id', auth()->guard('admin')->user()->id)
+                                ->orderBy('id', 'DESC')
+                                ->get();
+                            }
                         @endphp
                         <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}"
                              id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
@@ -48,68 +55,66 @@
                                             <th class="text-center">
                                                 #
                                             </th>
-                                            <th>{{ __('Image') }}</th>
-                                            <th>{{ __('Title') }}</th>
-                                            <th>{{ __('Category') }}</th>
-
-                                            <th>{{ __('In Breaking') }}</th>
-                                            <th>{{ __('In Slider') }}</th>
-                                            <th>{{ __('In Popular') }}</th>
-
-                                            <th>{{ __('Status') }}</th>
-                                            <th>{{ __('Action') }}</th>
+                                            <th>{{__('Image') }}</th>
+                                            <th>{{__('Title') }}</th>
+                                            <th>{{__('Category') }}</th>
+                                            @if (canAccess(['news status', 'news all-access']))
+                                                <th>{{__('In Breaking') }}</th>
+                                                <th>{{__('In Slider') }}</th>
+                                                <th>{{__('In Popular') }}</th>
+                                            @endif
+                                            <th>{{__('Status') }}</th>
+                                            <th>{{__('Action') }}</th>
                                         </tr>
                                         </thead>
                                         <tbody>
                                         @foreach ($news as $item)
                                             <tr>
                                                 <td>{{ $item->id }}</td>
-                                                <td>
+                                                <td >
                                                     <img src="{{ asset($item->image) }}" width="100" alt="">
                                                 </td>
 
                                                 <td>{{ $item->title }}</td>
                                                 <td>{{ $item->category->name }}</td>
-                                                <td>
-                                                    <label class="custom-switch mt-2">
-                                                        <input {{ $item->is_breaking_news === 1 ? 'checked' : '' }}
-                                                               data-id="{{ $item->id }}" data-name="is_breaking_news"
-                                                               value="1" type="checkbox"
-                                                               class="custom-switch-input toggle-status">
-                                                        <span class="custom-switch-indicator"></span>
-                                                    </label>
-                                                </td>
+                                                @if (canAccess(['news status', 'news all-access']))
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->is_breaking_news === 1 ? 'checked' : '' }}
+                                                                   data-id="{{ $item->id }}" data-name="is_breaking_news"
+                                                                   value="1" type="checkbox" class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
 
-                                                <td>
-                                                    <label class="custom-switch mt-2">
-                                                        <input {{ $item->show_at_slider === 1 ? 'checked' : '' }}
-                                                               data-id="{{ $item->id }}" data-name="show_at_slider"
-                                                               value="1" type="checkbox"
-                                                               class="custom-switch-input toggle-status">
-                                                        <span class="custom-switch-indicator"></span>
-                                                    </label>
-                                                </td>
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_slider === 1 ? 'checked' : '' }}
+                                                                   data-id="{{ $item->id }}" data-name="show_at_slider"
+                                                                   value="1" type="checkbox" class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
 
-                                                <td>
-                                                    <label class="custom-switch mt-2">
-                                                        <input {{ $item->show_at_popular === 1 ? 'checked' : '' }}
-                                                               data-id="{{ $item->id }}" data-name="show_at_popular"
-                                                               value="1" type="checkbox"
-                                                               class="custom-switch-input toggle-status">
-                                                        <span class="custom-switch-indicator"></span>
-                                                    </label>
-                                                </td>
-
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_popular === 1 ? 'checked' : '' }}
+                                                                   data-id="{{ $item->id }}" data-name="show_at_popular"
+                                                                   value="1" type="checkbox" class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+                                                @endif
 
                                                 <td>
                                                     <label class="custom-switch mt-2">
                                                         <input {{ $item->status === 1 ? 'checked' : '' }}
                                                                data-id="{{ $item->id }}" data-name="status"
-                                                               value="1" type="checkbox"
-                                                               class="custom-switch-input toggle-status">
+                                                               value="1" type="checkbox" class="custom-switch-input toggle-status">
                                                         <span class="custom-switch-indicator"></span>
                                                     </label>
                                                 </td>
+
 
                                                 <td>
                                                     <a href="{{ route('admin.news.edit', $item->id) }}"
@@ -118,10 +123,11 @@
                                                        class="btn btn-danger delete-item"><i
                                                             class="fas fa-trash-alt"></i></a>
                                                     <a href="{{ route('admin.news-copy', $item->id) }}"
-                                                       class="btn btn-primary"><i class="fas fa-copy"></i></a>
+                                                       class="btn btn-primary"><i class="fas fa-copy"></i></i></a>
                                                 </td>
                                             </tr>
                                         @endforeach
+
 
                                         </tbody>
                                     </table>
@@ -132,6 +138,7 @@
 
                 </div>
             </div>
+
 
         </div>
     </section>
